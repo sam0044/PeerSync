fef18bfab9906c08547e881d6b6e3e33133d1b46
Sam Goyal <samgoyal0044@gmail.com>
2025-01-10 12:12:46 -0800

    added middleware for session validation

---
+++ peersync/src/app/api/sessions/validate/[sessionId]/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { getSession,deleteSession } from "@/lib/session";
+
+export async function GET(request: Request, { params }: { params: { sessionId: string } }) {
+    try{
+        const sessionId = params.sessionId;
+        const session = await getSession(sessionId);
+        if (!session) {
+            return NextResponse.json({ 
+              valid: false, 
+              message: "Invalid session" 
+            }, { status: 404 });
+          }
+      
+          if (Date.now() > session.expiresAt) {
+            await deleteSession(params.sessionId);
+            return NextResponse.json({ 
+              valid: false, 
+              message: "Session expired" 
+            }, { status: 410 });
+          }
+        return NextResponse.json({valid: true}, {status: 200});
+    } catch {
+        return NextResponse.json({ valid: false, message: "Server error"}, {status: 500});
+    }
+}
