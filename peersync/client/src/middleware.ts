6ab40e10d01d2df360588e0f51a6bf2b2f8ab27e
Sam Goyal <samgoyal0044@gmail.com>
2025-01-12 20:49:45 -0800

    temporarily removing sesson validation for debugging

--- peersync/client/src/middleware.ts
+++ peersync/client/src/middleware.ts
@@ -1,27 +1,29 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest){
-    if(!request.nextUrl.pathname.startsWith('/receive')){
-        return NextResponse.next();
-    }
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function middleware(_request: NextRequest){
+    // if(!request.nextUrl.pathname.startsWith('/receive')){
+    //     return NextResponse.next();
+    // }
 
-    const sessionId = request.nextUrl.pathname.split('/').pop();
-    try{
-        const response = await fetch(`${request.nextUrl.origin}/api/sessions/validate/${sessionId}`, {method: 'GET'});
-        const data = await response.json();
-        if(data.valid){
-            return NextResponse.next();
-        }
-        const redirectUrl = new URL('/error', request.url);
-        redirectUrl.searchParams.set('type', data.message === 'Session expired' ? 'expired-session' : 'invalid-session');
-        return NextResponse.redirect(redirectUrl);
+    // const sessionId = request.nextUrl.pathname.split('/').pop();
+    // try{
+    //     const response = await fetch(`${request.nextUrl.origin}/api/sessions/validate/${sessionId}`, {method: 'GET'});
+    //     const data = await response.json();
+    //     if(data.valid){
+    //         return NextResponse.next();
+    //     }
+    //     const redirectUrl = new URL('/error', request.url);
+    //     redirectUrl.searchParams.set('type', data.message === 'Session expired' ? 'expired-session' : 'invalid-session');
+    //     return NextResponse.redirect(redirectUrl);
 
-    } catch {
-        const redirectUrl = new URL('/error', request.url);
-        redirectUrl.searchParams.set('type', 'invalid-session');
-        return NextResponse.redirect(redirectUrl);
-    }
+    // } catch {
+    //     const redirectUrl = new URL('/error', request.url);
+    //     redirectUrl.searchParams.set('type', 'invalid-session');
+    //     return NextResponse.redirect(redirectUrl);
+    // }
+    return NextResponse.next();
 }
 
 
